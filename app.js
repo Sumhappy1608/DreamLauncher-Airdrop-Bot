6faf397b27fd29b606358e8bc5dc24ef9e4ea139
Nguyễn Đình Khôi <49549370+ngdkhoi@users.noreply.github.com>
2022-02-16 10:24:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const Connect = require('./config/database')
+const indexRouter = require('./routes/index');
 require('./bot/bot')
 
 const app = express();
@@ -21,6 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
